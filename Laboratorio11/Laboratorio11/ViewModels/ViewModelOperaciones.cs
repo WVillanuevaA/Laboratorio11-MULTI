9c724bd390ee04f38c177d38ec0e055ecb68f837
Your Name <you@example.com>
2023-11-25 18:09:46 -0500

    Agregar archivos de proyecto.

---
+++ Laboratorio11/Laboratorio11/ViewModels/ViewModelOperaciones.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Laboratorio11.ViewModels
+{
+    public class ViewModelOperaciones : ViewModelBase
+    {
+        int valorA;
+
+        public int ValorA
+        {
+            get { return valorA; }
+            set
+            {
+                if (valorA != value)
+                {
+                    valorA = value;
+                    OnPropertyChanged();
+                }
+            }
+
+        }
+
+        int valorB;
+
+        public int ValorB
+        {
+            get { return valorB; }
+            set
+            {
+                if (valorB != value)
+                {
+                    valorB = value;
+                    OnPropertyChanged();
+                }
+            }
+
+        }
+        int valorC;
+
+        public int ValorC
+        {
+            get { return valorC; }
+            set
+            {
+                if (valorC != value)
+                {
+                    valorC = value;
+                    OnPropertyChanged();
+                }
+            }
+
+        }
+        int resultadoSuma;
+
+        public int ResultadoSuma
+        {
+            get { return resultadoSuma; }
+            set
+            {
+                if (resultadoSuma != value)
+                {
+                    resultadoSuma = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        int resultadoMultiplica;
+        public int ResultadoMultiplica
+        {
+            get { return resultadoMultiplica; }
+            set
+            {
+                if (resultadoMultiplica != value)
+                {
+                    resultadoMultiplica = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public ICommand Sumar { protected set; get; }
+        public ICommand Multiplicar { protected set; get; }
+
+
+
+        public ViewModelOperaciones()
+        {
+            Sumar = new Command(() =>
+            {
+                ResultadoSuma = ValorA + ValorB + ValorC;
+            });
+
+            Multiplicar = new Command(() =>
+            {
+                ResultadoMultiplica = ValorA * ValorB * ValorC;
+            });
+
+        }
+    }
+}
---
+++ Laboratorio11/Laboratorio11/Views/Operacions.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Laboratorio11.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Laboratorio11.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Operacions : ContentPage
+    {
+        public Operacions()
+        {
+            InitializeComponent();
+            this.BindingContext = new ViewModelOperaciones();
+        }
+    }
+}
